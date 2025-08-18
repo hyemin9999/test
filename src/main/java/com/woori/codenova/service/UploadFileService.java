ea92855784b23483e54e5fdb89ef358c3daadda6
hyemin9999 <hmlim9999@gmail.com>
2025-08-18 18:15:43 +0900

    toast ui editor 이미지 서버 저장 및 db처리(게시글/공지사항 테이블 연동, 삭제 처리 기능 작업 필)

---
+++ src/main/java/com/woori/codenova/service/UploadFileService.java
@@ -0,0 +1,11 @@
+package com.woori.codenova.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UploadFileService {
+
+}
