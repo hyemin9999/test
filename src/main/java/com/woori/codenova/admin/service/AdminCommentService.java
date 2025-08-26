2b08caceab5b2876799f7967a3fcb9948e170bc4
hyemin9999 <hmlim9999@gmail.com>
2025-08-26 19:07:52 +0900

    소스 정리

--- src/main/java/com/woori/codenova/admin/service/AdminCommentService.java
+++ src/main/java/com/woori/codenova/admin/service/AdminCommentService.java
@@ -1,11 +1,6 @@
 package com.woori.codenova.admin.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import com.woori.codenova.entity.Comment;
-import com.woori.codenova.repository.CommentRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,10 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class AdminCommentService {
 
-	private final CommentRepository commentRepository;
+//	private final CommentRepository commentRepository;
 
-	// 사용자가 작성한 글 목록 반환
-	public List<Comment> getListByAuthorId(Long id) {
-		return commentRepository.getByAuthorId(id);
-	}
+//	// 사용자가 작성한 글 목록 반환
+//	public List<Comment> getListByAuthorId(Long id) {
+//		return commentRepository.getByAuthorId(id);
+//	}
 }
--- src/main/java/com/woori/codenova/repository/BoardRepository.java
+++ src/main/java/com/woori/codenova/repository/BoardRepository.java
@@ -2,12 +2,10 @@ package com.woori.codenova.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.woori.codenova.entity.Board;
 import com.woori.codenova.entity.SiteUser;
@@ -18,9 +16,10 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
 
 	Page<Board> findAll(Pageable pageable);
 
-	@Query(value = "SELECT b FROM Board b WHERE b.author.id = :id", nativeQuery = true)
-	List<Board> getByAuthorId(@Param("id") Long id); // 작성한 게시글
+//	@Query(value = "SELECT b FROM Board b WHERE b.author.id = :id", nativeQuery = true)
+//	List<Board> getByAuthorId(@Param("id") Long id); // 작성한 게시글
 
+	// 작성자
 	List<Board> findByAuthor(SiteUser author);
 
 }
