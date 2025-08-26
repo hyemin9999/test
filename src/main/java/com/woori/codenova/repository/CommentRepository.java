06635eb2a25b716e8112be37038b896efcc6d194
hyemin9999 <hmlim9999@gmail.com>
2025-08-26 19:07:52 +0900

    소스 정리

--- src/main/java/com/woori/codenova/repository/CommentRepository.java
+++ src/main/java/com/woori/codenova/repository/CommentRepository.java
@@ -2,15 +2,17 @@ package com.woori.codenova.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.woori.codenova.entity.Comment;
+import com.woori.codenova.entity.SiteUser;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-	@Query("SELECT c FROM Comment c LEFT JOIN SiteUser u ON c.author = u WHERE u.id = :id")
-	List<Comment> getByAuthorId(@Param("id") Long id);
+//	@Query("SELECT c FROM Comment c LEFT JOIN SiteUser u ON c.author = u WHERE u.id = :id")
+//	List<Comment> getByAuthorId(@Param("id") Long id);
+
+	// 작성자
+	List<Comment> findByAuthor(SiteUser author);
 
 }
--- src/main/java/com/woori/codenova/repository/RoleRepository.java
+++ src/main/java/com/woori/codenova/repository/RoleRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.woori.codenova.dto.AdminRoleDto;
 import com.woori.codenova.entity.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
@@ -20,10 +19,10 @@ public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 	Optional<Role> findByGrade(Integer grade);
 
-	// 회원 관리에서 사용할 역할 목록 반환 처리 - 이름순으로 정렬
-	@Query("SELECT new com.woori.codenova.dto.AdminRoleDto(r.id, r.name, false) FROM Role r "
-			+ "WHERE r.grade != 1 ORDER BY name ASC ")
-	List<AdminRoleDto> findAllByAuthority();
+//	// 회원 관리에서 사용할 역할 목록 반환 처리 - 이름순으로 정렬
+//	@Query("SELECT new com.woori.codenova.dto.AdminRoleDto(r.id, r.name, false) FROM Role r "
+//			+ "WHERE r.grade != 1 ORDER BY name ASC ")
+//	List<AdminRoleDto> findAllByAuthority();
 
 	@Query(value = "SELECT * FROM Role " + "WHERE grade != 1 ORDER BY name ASC ", nativeQuery = true)
 	List<Role> findAllByGrade();
